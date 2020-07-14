81df8bbd6ad3b70a30ff682092a6d9422077c2b7
fly-gad <16619813091@163.com>
2020-07-14 17:30:57 +0800

    yeminma

---
+++ pages/index/index.js
@@ -0,0 +1,91 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navList: []
+  },
+  /**
+   * 小程序内置请求方法
+   */
+  getNavList() {
+    let that = this;
+    wx.request({
+      url: 'http://mock-api.com/vKVNkXK8.mock/navList',
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        console.log(res.data.data.navList);
+        if (res.data.code == 0) {
+          that.setData({
+            navList: res.data.data.navList
+
+          })
+        }
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getNavList()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
